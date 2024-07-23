099363d7c391bb0db36430b6f44e706fe24e8564
d0tm4cul4 <d0t@idkhouse.com>
2024-07-23 14:03:03 -0500

    a

--- main.py
+++ main.py
@@ -1,66 +1,6 @@
 import pyautogui
 import time
-from PIL import Image
-import numpy as np
-import pandas as pd
 import random
-
-
-def valoresImg():
-    image_path = "./principal_pantalla.jpg"
-    image = Image.open(image_path)
-    image = image.convert("L")
-    image_array = np.array(image)
-    df = pd.DataFrame(image_array)
-    csv_path = "./valorImg.csv"
-    df.to_csv(csv_path, index=False, header=False)
-    print(f"La imagen ha sido convertida y guardada en {csv_path}")
-
-
-a = [
-    {
-        "3": [1, 0],
-        "6": [2, 0],
-        "9": [3, 0],
-        "12": [4, 0],
-        "15": [5, 0],
-        "18": [6, 0],
-        "21": [7, 0],
-        "24": [8, 0],
-        "27": [9, 0],
-        "30": [10, 0],
-        "33": [11, 0],
-        "36": [12, 0],
-    },
-    {
-        "2": [1, 1],
-        "5": [2, 1],
-        "8": [3, 1],
-        "11": [4, 1],
-        "14": [5, 1],
-        "17": [6, 1],
-        "20": [7, 1],
-        "23": [8, 1],
-        "26": [9, 1],
-        "29": [10, 1],
-        "32": [11, 1],
-        "35": [12, 1],
-    },
-    {
-        "1": [1, 2],
-        "4": [2, 2],
-        "7": [3, 2],
-        "10": [4, 2],
-        "13": [5, 2],
-        "16": [6, 2],
-        "19": [7, 2],
-        "22": [8, 2],
-        "25": [9, 2],
-        "28": [10, 2],
-        "31": [11, 2],
-        "34": [12, 2],
-    },
-]
 
 
 def skipNotification(perC):
@@ -127,13 +67,18 @@ def moveMouse(nC):
             iW_one += 1
     nCmov.sort()
     iW_two = 0
+    average_time = 13
+    time_per_Move = [random.random() for _ in range(nC)]
+    current_sum = sum(time_per_Move)
+    time_per_Move = [x * average_time / current_sum for x in time_per_Move]
+
     while iW_two < nC:
         pyautogui.moveTo(dictValuesC[nCmov[iW_two]][0], dictValuesC[nCmov[iW_two]][1])
         # pyautogui.click()
         print(
             f"i:{iW_two}({nCmov[iW_two]+1})->x:{dictValuesC[nCmov[iW_two]][0]},y:{dictValuesC[nCmov[iW_two]][1]}"
         )
-        # time.sleep(1)
+        time.sleep(time_per_Move[iW_two])
         iW_two += 1
 
 
@@ -156,5 +101,3 @@ def mixFunctionC():
 
 
 mixFunctionC()
-
-# valoresImg()
